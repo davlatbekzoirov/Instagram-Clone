f5332abfbb55d282c5e63ba0117655eb12f3e3cb
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-30 01:17:24 +0500

    create shared app

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from shared.models import BaseModel
 
 ORDINARY_USER, MANAGER, ADMIN = ('ordinary_user', 'manager', 'admin')
 VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
 NEW, CODE_VERIFIED, DONE, PHOTO_STEP = ("new", "code", "done", "photo_step")
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     USER_ROLES = (
         (ORDINARY_USER, ORDINARY_USER),
         (MANAGER, MANAGER),
@@ -27,6 +28,3 @@ class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
     phone_number = models.CharField(max_length=13, unique=True, null=True, blank=True)
     photo = models.ImageField(upload_to="users/photos", null=True, blank=True)
-    created_time =
-    update_time =
-    id =
