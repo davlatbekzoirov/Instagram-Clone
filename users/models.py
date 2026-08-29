2a0da5d2e900a8adc52620b8613b7c5d1ae07151
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-30 01:10:54 +0500

    create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+ORDINARY_USER, MANAGER, ADMIN = ('ordinary_user', 'manager', 'admin')
+VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
+NEW, CODE_VERIFIED, DONE, PHOTO_STEP = ("new", "code", "done", "photo_step")
+
+class User(AbstractUser):
+    USER_ROLES = (
+        (ORDINARY_USER, ORDINARY_USER),
+        (MANAGER, MANAGER),
+        (ADMIN, ADMIN),
+    )
+    AUTH_TYPE_CHOICES = (
+        (VIA_PHONE, VIA_PHONE),
+        (VIA_EMAIL, VIA_EMAIL),
+    )
+    AUTH_STATUS=(
+        (NEW, NEW),
+        (CODE_VERIFIED, CODE_VERIFIED),
+        (DONE, DONE),
+        (PHOTO_STEP, PHOTO_STEP)
+    )
+    user_roles = models.CharField(max_length=31, choices=USER_ROLES, default=ORDINARY_USER)
+    AUTH_TYPE = models.CharField(max_length=31, choices=AUTH_TYPE_CHOICES)
+    AUTH_STATUS = models.CharField(max_length=31, choices=AUTH_STATUS, default=NEW)
+    email = models.EmailField(max_length=255, unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, unique=True, null=True, blank=True)
+    photo = models.ImageField(upload_to="users/photos", null=True, blank=True)
+    created_time =
+    update_time =
+    id =
