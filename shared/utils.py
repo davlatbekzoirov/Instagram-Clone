9081d4ad6a735218f36ae9494ae22b77f15187f5
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-09-02 03:50:26 +0500

    created email send

--- shared/utils.py
+++ shared/utils.py
@@ -1,4 +1,8 @@
 import re
+import threading
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 from rest_framework.exceptions import ValidationError
 
 email_regex = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}\b")
@@ -14,3 +18,28 @@ def check_email_or_phone(email_or_phone):
             "success": False,
             "message": "Email yoki telefon raqamingiz noto'g'ri"
         })
+
+class EmailThread(threading.Thread):
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+class Email:
+    @staticmethod
+    def send_email(data):
+        email = EmailMessage(
+            subject=data['subject'],
+            body=data['body'],
+            to=[data['to_email']],
+        )
+        if data.get('content_type') == 'html':
+            email.content_subtype = 'html'
+        EmailThread(email).start()
+
+def send_email(email, code):
+    html_content = render_to_string(
+
+    )
